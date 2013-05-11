b2f8c614f5fb5dd6f14bae92f1bdf0d0b0a72dd9
Gareth Rushgrove <gareth@morethanseven.net>
2013-05-11 10:41:36 +0100

    test for timeout as otherwise causes errors

--- spec/defines/pull_spec.rb
+++ spec/defines/pull_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'docker::pull', :type => :define do
   let(:title) { 'base' }
-  it { should contain_exec('docker pull base') }
+  it { should contain_exec('docker pull base').with_timeout(0) }
 
  context 'with an invalid image name' do
     let(:title) { 'with spaces' }
