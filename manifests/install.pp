60f0ce580f1a4596255d91cbd469e7f8b626ebe9
James Carr <james@zapier.com>
2013-08-24 11:28:07 -0700

    Do not include the source repo

--- manifests/install.pp
+++ manifests/install.pp
@@ -9,23 +9,31 @@
 #   The package version to install, passed to ensure.
 #   Defaults to present.
 #
-class docker::install {
+class docker::install (
+  $tcp_bind = undef,
+  $unix_socket = 'unix:///var/run/docker.sock',
+){
   include apt
   validate_string($version)
   validate_re($::osfamily, '^Debian$', 'This module uses the docker apt repo and only works on Debian systems that support it.')
 
   apt::source { 'docker':
-    location          => 'https://get.docker.io/ubuntu',
-    release           => 'docker',
+    location          => 'https://get.docker.io/docker',
+    release           => 'ubuntu',
     repos             => 'main',
     required_packages => 'debian-keyring debian-archive-keyring',
     key               => 'A88D21E9',
     key_source        => 'http://get.docker.io/gpg',
-    pin               => '10'
+    pin               => '10',
+    include_src       => false,
   }
     
   package { 'lxc-docker':
     ensure  => $docker::version,
     require => Apt::Source['docker'],
+  }
+
+  file { '/etc/init/docker':
+    content => template('docker/etc/init/docker.conf.erb')
   }
 }
