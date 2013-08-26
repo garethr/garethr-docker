27fd9fe985abe5909f1f70d20c639949b372d21c
Elias Probst <mail@eliasprobst.eu>
2013-08-27 00:33:21 +0200

    Updated specs.

--- spec/classes/docker_spec.rb
+++ spec/classes/docker_spec.rb
@@ -11,8 +11,8 @@ describe 'docker', :type => :class do
   context 'with no parameters' do
     it { should include_class('apt') }
     it { should contain_package('lxc-docker').with_ensure('present') }
-    it { should contain_package('lxc-docker').with_require('Apt::Ppa[ppa:dotcloud/lxc-docker]') }
-    it { should contain_apt__ppa('ppa:dotcloud/lxc-docker') }
+    it { should contain_package('lxc-docker').with_require('Apt::Source[lxc-docker]') }
+    it { should contain_apt__source('lxc-docker') }
   end
 
   context 'with a custom version' do
