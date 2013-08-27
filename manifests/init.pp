d6feefe7869226bcd7e57899ae4ce2a2ca28986b
Elias Probst <mail@eliasprobst.eu>
2013-08-27 19:54:51 +0200

    Updated the validation message for the $::operatingsystem fact.

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,7 +14,7 @@ class docker(
 ) inherits docker::params {
 
   validate_string($version)
-  validate_re($::operatingsystem, '^Ubuntu$', 'This module uses PPA repos and only works with Debian based distros')
+  validate_re($::operatingsystem, '^Ubuntu$', 'This module works currently only with Ubuntu based distributions.')
 
   class { 'docker::install': } ->
   class { 'docker::config': } ~>
