96fd11845d6dbd84b894d8c5b960ed23a9956f83
Elias Probst <mail@eliasprobst.eu>
2013-09-04 22:39:13 +0200

    Updated specs to also check for the linux-image-extra package dependency.

--- spec/classes/docker_spec.rb
+++ spec/classes/docker_spec.rb
@@ -4,7 +4,7 @@ describe 'docker', :type => :class do
   let(:facts) { {
     :osfamily        => 'Debian',
     :lsbdistcodename => 'precise',
-    :kernelrelease   => 'linux-image-extra-3.8.0-29-generic'
+    :kernelrelease   => '3.8.0-29-generic'
   } }
 
   it { should include_class('docker::install') }
@@ -15,7 +15,7 @@ describe 'docker', :type => :class do
   context 'with no parameters' do
     it { should include_class('apt') }
     it { should contain_package('lxc-docker').with_ensure('present') }
-    it { should contain_package('lxc-docker').with_require('Apt::Source[docker]') }
+    it { should contain_package('lxc-docker').with_require(['Apt::Source[docker]', 'Package[linux-image-extra-3.8.0-29-generic]']) }
   end
 
   context 'with a custom version' do
