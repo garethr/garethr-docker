43cc6121cdbc835d881c4a49cbedd84f6c44f23b
James Carr <james@zapier.com>
2013-10-07 23:02:10 -0500

    added network bridge configuration option

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,6 +1,7 @@
 class docker::service (
-  $tcp_bind    = $docker::params::tcp_bind,
-  $socket_bind = $docker::params::socket_bind,
+  $tcp_bind      = $docker::params::tcp_bind,
+  $socket_bind   = $docker::params::socket_bind,
+  $network_brige = $docker::params::network_brige,
 ){
   service { 'docker':
     ensure     => running,
