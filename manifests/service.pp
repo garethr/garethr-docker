e09bdd732e58f2ad707408fe952a3dab3bebcf33
Hugh Esco <hesco@campaignfoundations.com>
2014-11-30 10:55:54 +0000

    Busr bug: Sanity check: system reboot is required to enable the new kernel

--- manifests/service.pp
+++ manifests/service.pp
@@ -90,7 +90,7 @@ class docker::service (
     default  => undef,
   }
 
-  if versioncmp( $::kernelversion, '3.8.0' ) > 0 {
+  if versioncmp( $::kernelversion, '3.8.0' ) >= 0 {
     service { 'docker':
       ensure     => $service_state,
       name       => $service_name,
