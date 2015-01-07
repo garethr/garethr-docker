7b67f075e5d269b2b5599cff09d3a207e15f65f1
Janos Feher <i.am@janosfeher.com>
2015-01-07 20:58:10 +0000

    Support for docker-exec added.

---
+++ lib/puppet/parser/functions/docker_exec_flags.rb
@@ -0,0 +1,21 @@
+module Puppet::Parser::Functions
+  # Transforms a hash into a string of docker exec flags
+  newfunction(:docker_exec_flags, :type => :rvalue) do |args|
+    opts = args[0] || {}
+    flags = []
+
+    if opts['detach']
+      flags << '--detach=true'
+    end
+    
+    if opts['interactive']
+      flags << '--interactive=true'
+    end
+    
+    if opts['tty']
+      flags << '--tty=true'
+    end
+
+    flags.flatten.join(" ")
+  end
+end
