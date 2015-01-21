297705f956c7555a5b9f733118b2231c8ba0e696
Thomas Krille <t.krille@tarent.de>
2015-01-21 18:43:40 +0100

    [change] enforce that containers have a name
    removed `use_name` parameter to accomplish this.

--- spec/defines/run_spec.rb
+++ spec/defines/run_spec.rb
@@ -66,11 +66,6 @@ require 'spec_helper'
       it { should contain_file(initscript).with_content(/-lxc-conf=\"lxc.cgroup.memory.memsw.limit_in_bytes=536870912\"/) }
     end
 
-    context 'when `use_name` is true' do
-      let(:params) { {'command' => 'command', 'image' => 'base', 'use_name' => true } }
-      it { should contain_file(initscript).with_content(/ --name sample /) }
-    end
-
     context 'when stopping the service' do
       let(:params) { {'command' => 'command', 'image' => 'base', 'running' => false} }
       it { should contain_service('docker-sample').with_ensure(false) }
