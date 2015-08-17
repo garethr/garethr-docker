5258af5a2e7c5ff235a4c4dd90716fcd58507d69
paschdan <paschdan@gmail.com>
2015-08-17 15:57:13 +0200

    also find stopped containers when restart is set.

--- manifests/run.pp
+++ manifests/run.pp
@@ -146,7 +146,7 @@ define docker::run(
 
     exec { "run ${title} with docker":
       command     => "${docker_command} run -d ${docker_run_flags} --cidfile=${cidfile} ${image} ${command}",
-      unless      => "docker ps --no-trunc | grep `cat ${cidfile}`",
+      unless      => "docker ps --no-trunc -a | grep `cat ${cidfile}`",
       environment => 'HOME=/root',
       path        => ['/bin', '/usr/bin'],
     }
