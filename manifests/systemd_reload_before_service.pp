9105bdb07d8799a9f8ff5079c13c811592abac71
Wouter Scheele <w.scheele@secondfloor.com>
2016-01-18 23:43:05 +0100

    comply with stricter arrow alignment lint check

--- manifests/systemd_reload_before_service.pp
+++ manifests/systemd_reload_before_service.pp
@@ -6,9 +6,9 @@
 # the 'docker-systemd-reload' exec cannot be used to protect the service resource as it will introduce a dependency cycle through docker::run of containers.
 class docker::systemd_reload_before_service {
   exec { 'docker-systemd-reload-before-service':
-    path        => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
-    command     => 'systemctl daemon-reload',
-    before      => Service['docker'],
-    unless      => 'systemctl status docker'
+    path    => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
+    command => 'systemctl daemon-reload',
+    before  => Service['docker'],
+    unless  => 'systemctl status docker'
   }
 }
