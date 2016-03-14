8afc507417a0c8c1ca01fa7d4a6e3c56d0678fb6
tiago <tiago.nobre@mendeley.com>
2016-03-14 18:25:05 +0000

    remove not need checks

--- spec/defines/image_spec.rb
+++ spec/defines/image_spec.rb
@@ -83,6 +83,16 @@ describe 'docker::image', :type => :define do
     it { should contain_exec('docker build --build-arg FOO=bar -t base /tmp/docker_images/test1') }
   end
 
+  context 'with docker_dir => /tmp/docker_images/test1 and build_args => []}' do
+    let(:params) { { 'docker_dir' => '/tmp/docker_images/test1', 'build_args' => [] } }
+    it { should contain_exec('docker build -t base /tmp/docker_images/test1') }
+  end
+
+  context 'with docker_dir => /tmp/docker_images/test1 and build_args => ""}' do
+    let(:params) { { 'docker_dir' => '/tmp/docker_images/test1', 'build_args' => "" } }
+    it { should contain_exec('docker build -t base /tmp/docker_images/test1') }
+  end
+
   context 'with docker_tar => /tmp/docker_tars/test1.tar' do
     let(:params) { { 'docker_tar' => '/tmp/docker_tars/test1.tar' } }
     it { should contain_exec('docker load -i /tmp/docker_tars/test1.tar') }
