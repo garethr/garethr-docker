afcce090d633946883232da6d0255781f237161b
Gareth Rushgrove <gareth@morethanseven.net>
2016-03-18 10:03:31 +0000

    Fix for the acceptance tests on RHEL

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -23,6 +23,9 @@ RSpec.configure do |c|
     # Install module and dependencies
     puppet_module_install(:source => proj_root, :module_name => 'docker')
     hosts.each do |host|
+      # Due to RE-6764, running yum update renders the machine unable to install
+      # other software. Thus this workaround.
+      on(host, 'mv /etc/yum.repos.d/redhat.repo /etc/yum.repos.d/internal-mirror.repo')
       on(host, 'yum update -y -q') if fact_on(host, 'osfamily') == 'RedHat'
 
       on host, puppet('module', 'install', 'puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
