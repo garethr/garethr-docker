82c121fbd93eb21bb8450f7f85bd0b999742396a
Topher Dick <tdick@cas.org>
2016-04-27 07:41:44 -0400

    Makes small adjustments based on review recommendations Adds unit tests for the change

--- manifests/compose.pp
+++ manifests/compose.pp
@@ -13,23 +13,24 @@
 #   The version of Docker Compose to install.
 #   Defaults to the value set in $docker::params::compose_version
 #
-# [*install_from*]
+# [*install_url*]
 #   The URL to use to install docker-compose.
 #   Defaults to the URL recommended by https://docs.docker.com/compose/install/
 #
 class docker::compose(
   $ensure = 'present',
   $version = $docker::params::compose_version,
-  $install_from = "https://github.com/docker/compose/releases/download/${version}/docker-compose-${::kernel}-x86_64"
+  $install_url = "https://github.com/docker/compose/releases/download/${version}/docker-compose-${::kernel}-x86_64"
 ) inherits docker::params {
   validate_string($version)
   validate_re($ensure, '^(present|absent)$')
+  validate_re($install_url, '^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$')
 
   if $ensure == 'present' {
     exec { "Install Docker Compose ${version}":
       path    => '/usr/bin/',
       cwd     => '/tmp',
-      command => "curl -s -L ${install_from} > /usr/local/bin/docker-compose-${version}",
+      command => "curl -s -L ${install_url} > /usr/local/bin/docker-compose-${version}",
       creates => "/usr/local/bin/docker-compose-${version}"
     } ->
     file { "/usr/local/bin/docker-compose-${version}":
---
+++ spec/unit/compose_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe 'docker::compose', :type => :class do
+  it { is_expected.to compile }
+
+  let(:facts) { {:kernel => 'Linux'} }
+  let(:params) { {:version => '1.5.2'} }
+
+  context 'when no install_url is provided' do
+    it { is_expected.to contain_class('docker::compose').with_install_url(
+           'https://github.com/docker/compose/releases/download/1.5.2/docker-compose-Linux-x86_64') }
+    it { is_expected.to contain_exec('Install Docker Compose 1.5.2').with_command('curl -s -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-Linux-x86_64 > /usr/local/bin/docker-compose-1.5.2') }
+  end
+
+  context 'when a new install_url is provided' do
+    let(:params) { {:install_url => 'http://example.com/docker/compose/download/file'} }
+
+    it { is_expected.to compile }
+
+    it { is_expected.to contain_class('docker::compose').with_install_url(
+           'http://example.com/docker/compose/download/file')
+    }
+    it { is_expected.to contain_exec('Install Docker Compose 1.5.2').with_command('curl -s -L http://example.com/docker/compose/download/file > /usr/local/bin/docker-compose-1.5.2') }
+  end
+
+  context 'when install_url is not a url' do
+    let(:params) { {:install_url => 'this is not a valid url' } }
+
+    it do
+      expect {
+        is_expected.to compile
+      }.to raise_error(/does not match/)
+    end
+  end
+
+end
