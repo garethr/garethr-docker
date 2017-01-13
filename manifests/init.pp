b5164917863aeb304eaa6465df316e015f89121c
Eyal Lupu <eyal.lupu@gmail.com>
2017-01-13 16:36:01 +0000

    Formatting documentation a bit (just for kicking a new build as github was done breaking the build ...)

--- manifests/init.pp
+++ manifests/init.pp
@@ -345,7 +345,8 @@
 #   Extend the pool by specified percentage when threshold is hit.
 #
 # [*service_unit_after*]
-#   Systemd service After element for Docker service (will be added to override file)
+#   Systemd service After option for Docker service (will be added to service 
+#   override file)
 class docker(
   $version                           = $docker::params::version,
   $ensure                            = $docker::params::ensure,
