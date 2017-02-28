91e2da755e790a8c1569bafc9d75fbc1b7af2952
Elliot Huffman <thelich2@gmail.com>
2017-02-28 10:15:33 -0500

    depreciation update
    Based upon depreciation messages, syntax was updated to reflect new commands.

--- manifests/repos.pp
+++ manifests/repos.pp
@@ -17,14 +17,17 @@ class docker::repos {
           $key_source = $docker::package_key_source
           $package_key = $docker::package_key
         }
+        package { 'debian-keyring': ensure => 'installed' } ->
+        package { 'debian-archive-keyring': ensure => 'installed' } ->
         apt::source { 'docker':
           location          => $location,
           release           => $docker::package_release,
           repos             => $docker::package_repos,
-          key               => $package_key,
-          key_source        => $key_source,
-          required_packages => 'debian-keyring debian-archive-keyring',
-          include_src       => false,
+          key               => { 
+            id              => $package_key,
+            source          => $key_source, },
+          include           => {
+            src             => false, },
         }
         $url_split = split($location, '/')
         $repo_host = $url_split[2]
