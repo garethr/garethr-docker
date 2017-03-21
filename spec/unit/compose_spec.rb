7e23dc17f1d17d1371b4a9f8d03ace690695b61a
James McDonald <james@jamesmcdonald.com>
2017-03-21 13:03:25 +0100

    Add baseurl and rawurl to compose unit test

--- spec/unit/compose_spec.rb
+++ spec/unit/compose_spec.rb
@@ -40,4 +40,36 @@ describe 'docker::compose', :type => :class do
     end
   end
 
+  context 'when baseurl is provided' do
+    let(:params) { {:baseurl => 'http://fetch.compose.from/here', :version => '1.7.0'} }
+    it { is_expected.to contain_exec('Install Docker Compose 1.7.0').with_command(
+           'curl -s -L  http://fetch.compose.from/here/1.7.0/docker-compose-Linux-x86_64 > /usr/local/bin/docker-compose-1.7.0')
+    }
+  end
+
+  context 'when baseurl is not a url' do
+    let(:params) { {:baseurl => 'this is not a URL'} }
+    it do
+      expect {
+        is_expected.to compile
+      }.to raise_error(/does not match/)
+    end
+  end
+
+  context 'when rawurl is provided' do
+    let(:params) { {:rawurl => 'http://fetch.compose.from/over-there', :version => '1.7.0'} }
+    it { is_expected.to contain_exec('Install Docker Compose 1.7.0').with_command(
+           'curl -s -L  http://fetch.compose.from/over-there > /usr/local/bin/docker-compose-1.7.0')
+    }
+  end
+
+  context 'when rawurl is not a url' do
+    let(:params) { {:rawurl => 'this is not a URL'} }
+    it do
+      expect {
+        is_expected.to compile
+      }.to raise_error(/does not match/)
+    end
+  end
+
 end
