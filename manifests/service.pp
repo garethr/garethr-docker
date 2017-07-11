949d7296c458b6407b1b3f82ec55abdf9f2ed2b9
Markus Benning <markus.benning@atos.net>
2017-07-11 12:13:47 +0200

    Respect $service_name in service overrides
    This also changes the systemd service overrides to
    respect the $service_name.
    This is needed for compatibility with RedHats docker-latest
    packages.
    
    related #681

--- manifests/service.pp
+++ manifests/service.pp
@@ -98,6 +98,7 @@ class docker::service (
   $storage_config                    = $docker::storage_config,
   $storage_config_template           = $docker::storage_config_template,
   $storage_setup_file                = $docker::storage_setup_file,
+  $network_config                    = $docker::network_config,
   $service_provider                  = $docker::service_provider,
   $service_config                    = $docker::service_config,
   $service_config_template           = $docker::service_config_template,
