dc82d266f1ef30a474022bbf1bc59b45a032dc68
Henry <h.paulissen@tmg.nl>
2017-10-04 11:11:25 +0200

    Fixed apt::source call with newer puppetlabs-apt
    Signed-off-by: Henry <h.paulissen@tmg.nl>

--- manifests/repos.pp
+++ manifests/repos.pp
@@ -21,10 +21,10 @@ class docker::repos {
           location          => $location,
           release           => $docker::package_release,
           repos             => $docker::package_repos,
-          key               => $package_key,
-          key_source        => $key_source,
-          required_packages => 'debian-keyring debian-archive-keyring',
-          include_src       => false,
+          key               => {
+            'id'              => $package_key,
+            'source'          => $key_source,
+          },
         }
         $url_split = split($location, '/')
         $repo_host = $url_split[2]
